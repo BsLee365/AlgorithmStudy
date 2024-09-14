e69d9269049b0fddc462463216e3928985aa4166
이범석 <44566130+BsLee365@users.noreply.github.com>
2024-09-14 23:57:39 +0900

    상하좌우 문제 해결

--- 이코테/그리디/그리디5.py
+++ 이코테/그리디/그리디5.py
@@ -1,27 +1,34 @@
 # 여행가 위치 구하기
+# input 5
 # input R R R U D D
 # output 3 4
-
+maxNum = int(input())
 arrList = input().split()
 now = [1, 1]
 
 for i in arrList:
 
+    if i == 'L':
+        now[1] = now[1] - 1
+    elif i == 'R':
+        now[1] = now[1] + 1
+    elif i == 'U':
+        now[0] = now[0] - 1
+    elif i == 'D':
+        now[0] = now[0] + 1
+
     if now[0] <= 0:
         now[0] = 1
-        print(now[0])
         continue
     elif now[1] <= 0:
         now[1] = 1
         continue
-    else:
-        if i == 'L':
-            now[1] = now[1] - 1
-        elif i == 'R':
-            now[1] = now[1] + 1
-        elif i == 'U':
-            now[0] = now[0] - 1
-        elif i == 'D':
-            now[0] = now[0] + 1
+    elif now[1] >= maxNum:
+        now[1] = 5
+        continue
+    elif now[0] >= maxNum:
+        now[0] = 5
+        continue
+    print(now)
 
 print(now)
