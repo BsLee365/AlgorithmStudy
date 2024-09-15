a1c4707741e3c75b7b7e20181212eee71fd09008
이범석 <44566130+BsLee365@users.noreply.github.com>
2024-09-15 16:58:17 +0900

    .
    .

--- 이코테/그리디/그리디5.py
+++ 이코테/그리디/그리디5.py
@@ -2,12 +2,13 @@
 # input 5
 # input R R R U D D
 # output 3 4
+
 maxNum = int(input())
 arrList = input().split()
 now = [1, 1]
 
 for i in arrList:
-
+    # 이동 좌표로 움직임
     if i == 'L':
         now[1] = now[1] - 1
     elif i == 'R':
@@ -16,7 +17,7 @@ for i in arrList:
         now[0] = now[0] - 1
     elif i == 'D':
         now[0] = now[0] + 1
-
+    # 공간을 벗어나면 무시
     if now[0] <= 0:
         now[0] = 1
         continue
@@ -29,4 +30,28 @@ for i in arrList:
     elif now[0] >= maxNum:
         now[0] = 5
         continue
-print(now)
+print(now[0], now[1])
+
+
+##### 정석 풀이 #####
+n = int(input())
+x, y = 1, 1
+plans = input().split()
+
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+move_types = ['L', 'R', 'U', 'D']
+
+# 이동 계획을 하나씩 확인하기
+for plan in plans:
+    # 이동 후 좌표 구하기
+    for i in range(len(move_types)):
+        if plan == move_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+    # 공간을 벗어나는 경우 무시
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+    # 이동 수행
+    x, y, = nx, ny
+print(x, y)
